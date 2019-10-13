c4edbf0eaa76659dd5f8a5734e4711c3f6e993d9
Paweł Dziennik <dziennikp@gmail.com>
2019-10-13 16:12:16 +0200

    Set platform and swift version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Cards",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -26,5 +27,6 @@ let package = Package(
             name: "CardsTests",
             dependencies: ["Cards"]
         ),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
